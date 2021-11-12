15cf120fb88cc7399c7d4a1a6f8a873af6926606
Borja VG <borja.velasco@gigigo.com>
2021-11-12 01:18:30 +0100

    Added detail intent

--- app/src/main/java/es/borjavg/thingiverse/features/popular/ui/PopularThingsFragment.kt
+++ app/src/main/java/es/borjavg/thingiverse/features/popular/ui/PopularThingsFragment.kt
@@ -1,9 +1,7 @@
 package es.borjavg.thingiverse.features.popular.ui
 
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
