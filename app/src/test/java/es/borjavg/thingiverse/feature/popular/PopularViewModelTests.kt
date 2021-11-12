caf2305f75ea4df6a5602a17e4df11227708da97
Borja VG <borja.velasco@gigigo.com>
2021-11-12 10:31:09 +0100

    Added viewAction tests

--- app/src/test/java/es/borjavg/thingiverse/feature/popular/PopularViewModelTests.kt
+++ app/src/test/java/es/borjavg/thingiverse/feature/popular/PopularViewModelTests.kt
@@ -1,17 +1,17 @@
 package es.borjavg.thingiverse.feature.popular
 
 import androidx.lifecycle.Observer
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.*
 import es.borjavg.domain.Either
 import es.borjavg.domain.Left
 import es.borjavg.domain.Right
 import es.borjavg.domain.models.Thing
 import es.borjavg.thingiverse.base.BaseViewModelTests
 import es.borjavg.thingiverse.base.TestObserver
+import es.borjavg.thingiverse.features.main.ui.models.toPresentation
 import es.borjavg.thingiverse.features.popular.presentation.PopularThingsViewModel
+import es.borjavg.thingiverse.features.popular.presentation.PopularViewAction
+import es.borjavg.thingiverse.features.popular.presentation.PopularViewIntent
 import es.borjavg.thingiverse.features.popular.presentation.PopularViewState
 import org.junit.Assert
 import org.junit.Test
@@ -51,6 +51,21 @@ class PopularViewModelTests : BaseViewModelTests() {
         viewModel.load()
 
         Assert.assertTrue(observer.lastValue.items.isNotEmpty())
+    }
+
+    @Test
+    fun `GIVEN loaded items WHEN item is clicked THEN items detail is shown`() {
+        val viewModel = buildViewModel()
+        val observer = mock<Observer<PopularViewAction>>()
+        val captor = argumentCaptor<PopularViewAction>()
+        val thingModel = mockData.first().toPresentation()
+        viewModel.viewActions.observeForever(observer)
+
+        viewModel.load()
+        viewModel.sendIntent(PopularViewIntent.OnThingClick(thingModel))
+
+        verify(observer, atLeastOnce()).onChanged(captor.capture())
+        Assert.assertTrue(captor.lastValue is PopularViewAction.OpenThingDetail)
     }
 
     private val mockData = (0..15).map { index ->
